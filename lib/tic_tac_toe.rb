6b01320f039d2b59e34a3d0c1d32aa9bd9515a12
Marc Poppy <poppy.marc@gmail.com>
2019-05-04 19:25:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -107,11 +107,12 @@ def winner(board)
   end
 end
 
-#n
+#congratulate winning player X or O
 def play(board)
   turn_count = turn_count(board) #returns the number of turns
   while turn_count < 9 && over?(board) == false && draw?(board) == false
     turn(board)
     turn_count += 1
   end
+  puts "Congratulations #{winner(board)}"
 end
